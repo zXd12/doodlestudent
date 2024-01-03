e41528dd1f7c843827389d637833be003d0b7090
zXd12 <foucherewen@gmail.com>
2024-01-03 23:36:38 +0100

    added theme css files

--- front/src/app/theme-service.service.ts
+++ front/src/app/theme-service.service.ts
@@ -5,8 +5,12 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class ThemeService {
+
   private darkMode = new BehaviorSubject<boolean>(false);
   darkMode$ = this.darkMode.asObservable();
+
+  private theme = new BehaviorSubject<string>('light');
+  theme$ = this.theme.asObservable();
 
   constructor() {
     this.darkMode.next(localStorage.getItem('dark-mode') === 'true');
@@ -16,10 +20,25 @@ export class ThemeService {
     const newDarkMode = !this.darkMode.value;
     this.darkMode.next(newDarkMode);
     localStorage.setItem('dark-mode', newDarkMode.toString());
-    if (newDarkMode == true) {
-      document.body.classList.add('dark-mode');
+    this.toggleTheme(newDarkMode ? 'dark' : 'light');
+  }
+
+  toggleTheme(themeName: string) {
+    this.theme.next(themeName);
+    localStorage.setItem('theme', themeName);
+    document.body.className = themeName;
+  
+    const themeLink = document.getElementById('theme-link') as HTMLLinkElement;
+    if (themeLink) {
+      // If the link element already exists, update its href
+      themeLink.href = `themes/${themeName}.css`;
     } else {
-      document.body.classList.remove('dark-mode');
+      // If the link element doesn't exist, create it
+      const newThemeLink = document.createElement('link');
+      newThemeLink.id = 'theme-link';
+      newThemeLink.rel = 'stylesheet';
+      newThemeLink.href = `themes/${themeName}.css`;
+      document.head.appendChild(newThemeLink);
     }
   }
 }
